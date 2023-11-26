2d9cc2ddb1ea2e5033125380b60115a28bcbd3cb
Star World <96512249+TheStarWorld@users.noreply.github.com>
2023-11-25 23:45:36 -0700

    changes

--- converter/converter.py
+++ converter/converter.py
@@ -6,13 +6,20 @@ from django.conf import settings
 wp_voxel_list = []
 wp_xaero_list = []
 
+possible_dimensions = ["overworld","the_nether","the_end"]
 
 def convert_to_xaero(file):
+    wp_voxel_list = [] # resetting both lists seems to fix the index out of range error 
+    wp_xaero_list = []
     # filename = file.file.name
     filename = file.name
     # file = os.path.join(settings.MEDIA_ROOT, filename)
     # file_path = f'{settings.MEDIA_ROOT}{filename}_converted.txt'
-    out_txt = ''
+    out_txt = {
+        "overworld": '',
+        "the_nether": '',
+        "the_end": ''
+    }
     for line in file:
         line = line.decode()
         wp_voxel_list.append(line.strip('\n'))
@@ -34,13 +41,16 @@ def convert_to_xaero(file):
         z = wp[2].split(':')[1]
         enabled = wp[4].split(':')[1]
         color = hex(random.randint(0, 0xf))[2:]
-        dimension = wp[10].split(':')[1]
-        if enabled == 'true' and dimension == 'overworld#':
-            x_wp = f'waypoint:{name}:{name[0]}:{x}:{y}:{z}:{color}:false:0:gui.xaero_default:false:0:false\n'
-            out_txt = out_txt + x_wp
-        elif enabled == 'false' and dimension == 'overworld#':
-            x_wp = f'waypoint:{name}:{name[0]}:{x}:{y}:{z}:{color}:true:0:gui.xaero_default:false:0:false\n'
-            out_txt = out_txt + x_wp
+        dimensions = wp[10].split(':')[1]
+        dimensions = dimensions.split('#')
+        for dim_i in dimensions: # voxel waypoints can have multiple dimensions sometimes
+            if dim_i in possible_dimensions: # added support for output of all 3 vanilla dimensions
+                if enabled == 'true':
+                    x_wp = f'waypoint:{name}:{name[0]}:{x}:{y}:{z}:{color}:false:0:gui.xaero_default:false:0:false\n'
+                    out_txt[dim_i] = out_txt[dim_i] + x_wp
+                elif enabled == 'false':
+                    x_wp = f'waypoint:{name}:{name[0]}:{x}:{y}:{z}:{color}:true:0:gui.xaero_default:false:0:false\n'
+                    out_txt[dim_i] = out_txt[dim_i] + x_wp
 
         # f = open(file_path, 'a')
         # f.write(out_txt)
